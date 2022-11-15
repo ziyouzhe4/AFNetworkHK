08c6f9719a8f1a1e141f3b02859cf1cb3a04a33d
majianjie <majianjie@didiglobal.com>
2022-11-15 16:28:59 +0800

    add file

---
+++ Example/AFNetworkHKBinary/Test.h
@@ -0,0 +1,18 @@
+//
+//  Test.h
+//  AFNetworkHK
+//
+//  Created by majianjie on 2022/11/5.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Test : NSObject
+
+- (void)eat;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Example/AFNetworkHKBinary/Test.m
@@ -0,0 +1,18 @@
+//
+//  Test.m
+//  AFNetworkHK
+//
+//  Created by majianjie on 2022/11/5.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+- (void)eat{
+
+    NSLog(@"%s",__func__);
+    
+}
+
+@end
